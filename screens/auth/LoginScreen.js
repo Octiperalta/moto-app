6d7c6668c7547b7bb7e21d761917747ac5bbf48d
Octavio Peralta <octi.pro20@gmail.com>
2021-10-20 12:05:14 -0300

    other minor detail

--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -12,8 +12,8 @@ const LoginScreen = () => {
   const dispatch = useDispatch();
   const modal = useSelector(state => state.modal);
 
-  const [email, setEmail] = useState("asd@gmail.com");
-  const [password, setPassword] = useState("123456789");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleClose = () => {
     dispatch(closeModal());
@@ -66,56 +66,3 @@ const LoginScreen = () => {
 export default LoginScreen;
 
 const styles = StyleSheet.create({});
-
-{
-  /* field 1*/
-}
-//  <View style={tw`mt-6`}>
-//  {/* label */}
-//  <View style={tw`flex-row items-center`}>
-//    <Feather name='user' size={17} color={tw.color("gray-500")} />
-//    <Text
-//      fontWeight='medium'
-//      style={tw`ml-1 text-base text-gray-500`}>
-//      Email
-//    </Text>
-//  </View>
-
-//  {/* input */}
-//  <TextInput
-//    style={tw.style(
-//      "mt-1 px-3 py-2 rounded-lg border border-gray-300 text-gray-700",
-//      { fontSize: 16 }
-//    )}
-//  />
-// </View>
-
-// {/* field 2*/}
-// <View style={tw`mt-6`}>
-//  {/* label */}
-//  <View style={tw`flex-row items-center`}>
-//    <Feather name='lock' size={17} color={tw.color("gray-500")} />
-//    <Text
-//      fontWeight='medium'
-//      style={tw`ml-1 text-base text-gray-500`}>
-//      Password
-//    </Text>
-//  </View>
-//  {/* password input */}
-//  <View
-//    style={tw`flex-row items-center rounded-lg border border-gray-300 pr-3 mt-1`}>
-//    <TextInput
-//      style={tw.style("px-3 py-2 text-gray-700 flex-1", {
-//        fontSize: 16,
-//      })}
-//      secureTextEntry={hide}
-//    />
-//    <TouchableOpacity onPress={() => setHide(state => !state)}>
-//      <Feather
-//        name='eye-off'
-//        size={18}
-//        color={tw.color("gray-500")}
-//      />
-//    </TouchableOpacity>
-//  </View>
-// </View>
